2074fa43bd01472fb2a57c7d7dd479d46293da30
dudu <duduyugi@gmail.com>
2021-09-08 12:18:05 -0300

    adiciona borda aos FeatureItens

--- padawans_desafio/lib/controller/feature_item.dart
+++ padawans_desafio/lib/controller/feature_item.dart
@@ -9,33 +9,37 @@ class FeatureItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Material(
-        color: Theme.of(context).primaryColor,
-        child: InkWell(
-          onTap: () => onClick(),
-          child: Container(
-            padding: EdgeInsets.all(8.0),
-            height: 100,
-            width: 150,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Icon(
-                  icon,
-                  color: Colors.white,
-                  size: 24.0,
-                ),
-                Text(
-                  name,
-                  style: TextStyle(
+    return Card(
+      shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      color: Theme.of(context).primaryColor,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Material(
+          color: Theme.of(context).primaryColor,
+          child: InkWell(
+            onTap: () => onClick(),
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              height: 100,
+              width: 150,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Icon(
+                    icon,
                     color: Colors.white,
-                    fontSize: 16.0,
+                    size: 20.0,
                   ),
-                )
-              ],
+                  Text(
+                    name,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
